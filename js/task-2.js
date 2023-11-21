de1648d4b9f72ff647115f45e107f46b7220efac
DenisG92 <140918212+DenisG92@users.noreply.github.com>
2023-11-21 21:48:14 +0300

    dz1

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@
 
 function getShippingMessage(country, price, deliveryFee) {
     let guestName = "country";
-    return "Shipping to ${country} will cost ${price + deliveryFee} credits";
+    return "$(country) $(price + deliveryFee)";
 }
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
